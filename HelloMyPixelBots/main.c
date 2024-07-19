7f08989f623c9c38172cbc04903097839cabc461
Kinesis <lordkinesis@naver.com>
2024-07-19 22:15:05 +0900

    :sparkles: Add fixed console window size feature to main.c

--- HelloMyPixelBots/main.c
+++ HelloMyPixelBots/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <Windows.h>
 
 int main() {
+    // 콘솔 창의 핸들을 가져오기.
+    HWND consoleWindow = GetConsoleWindow();
+    // 현재 창의 스타일을 가져오기.
+    LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+    // 크기 조절 박스와 최대화 박스 스타일을 제거하여 크기 조절을 비활성화하기.
+    style &= ~(WS_MAXIMIZEBOX | WS_SIZEBOX);
+    // 수정된 스타일을 설정하기.
+    SetWindowLong(consoleWindow, GWL_STYLE, style);
+    // 콘솔 창의 사이즈는 200 * 50 size로 지정하기.
+    system("mode con:cols=200 lines=50");
 
-	printf("Hello My PixelBots!");
 
-	return 0;
+
+    return 0;
 }
