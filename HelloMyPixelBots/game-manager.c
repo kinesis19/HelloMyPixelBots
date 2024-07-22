df338c24d23a6b78e4fc857d89527d0814b472c6
Kinesis <lordkinesis@naver.com>
2024-07-23 02:40:09 +0900

    :hammer: Create game-manager.c

---
+++ HelloMyPixelBots/game-manager.c
@@ -0,0 +1,23 @@
+// 보안 경고 무시하기.
+#define _CRT_SECURE_NO_WARNINGS
+
+// 헤더파일 선언하기.
+#include <stdio.h>
+#include <Windows.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void Setting_Color();
+void SettingCursor(int x, int y);
+
+void Setting_Color(enum ColorType color) {
+
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+
+}
+
+void SettingCursor(int x, int y) {
+    COORD coord = { x, y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
