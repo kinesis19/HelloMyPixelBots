9c638e842144ab7dfbe458ebadb0297acbf22d97
Kinesis <lordkinesis@naver.com>
2024-07-23 02:39:48 +0900

    :wrench: Create game-manager.h

---
+++ HelloMyPixelBots/game-manager.h
@@ -0,0 +1,22 @@
+#pragma once
+enum ColorType {
+	BLACK,
+	DARK_BLUE,
+	DARK_GREEN,
+	DARK_SKYBLUE,
+	DARK_RED,
+	DARK_PURPLE,
+	DARK_YELLOW,
+	GRAY,
+	DARK_GRAY,
+	BLUE,
+	GREEN,
+	SKYBLUE,
+	RED,
+	PURPLE,
+	YELLOW,
+	WHILE
+};
+
+void Setting_Color();
+void SettingCursor(int x, int y);
