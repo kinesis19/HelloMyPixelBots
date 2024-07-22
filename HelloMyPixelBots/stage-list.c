e7d28460d60159fc8b8e090601470b7bf9bb798c
Kinesis <lordkinesis@naver.com>
2024-07-23 01:56:21 +0900

    :hammer: Create stage-list.c

---
+++ HelloMyPixelBots/stage-list.c
@@ -0,0 +1,15 @@
+// 보안 경고 무시하기.
+#define _CRT_SECURE_NO_WARNINGS
+
+// 헤더파일 선언하기.
+#include <stdio.h>
+#include <Windows.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void Drawing_Playground_Chapter1_Stage1();
+
+void Drawing_Playground_Chapter1_Stage1() {
+
+}
