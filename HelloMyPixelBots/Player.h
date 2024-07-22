0a7eff6b765da5981d58a950096703811863ca6a
Kinesis <lordkinesis@naver.com>
2024-07-22 19:55:37 +0900

    :wrench: Create Player.h

---
+++ HelloMyPixelBots/Player.h
@@ -0,0 +1,13 @@
+#pragma once
+
+typedef struct _Player {
+
+	int credit; // Player의 재화.
+	int level; // Player의 레벨.
+	int Chapters[5][5]; // Player의 Chapter 및 Stage 클리어 정보. e.x) Chapters[0][0] : Chapter1의 Stage1을 가리킴.
+	
+}Player;
+
+extern Player player;
+
+void InitializingPlayer();
