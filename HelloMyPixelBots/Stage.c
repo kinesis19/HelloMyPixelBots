ec5a14c0aed86efdcfcbd8606f03c2b8086658ee
Kinesis <lordkinesis@naver.com>
2024-07-22 22:09:13 +0900

    :hammer: Create Stage.c

---
+++ HelloMyPixelBots/Stage.c
@@ -0,0 +1,13 @@
+// 보안 경고 무시하기.
+#define _CRT_SECURE_NO_WARNINGS
+
+// 헤더파일 선언하기.
+#include <stdio.h>
+#include <Windows.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void Stage() {
+
+}
