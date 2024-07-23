83ac8df3410e5a7e0903d9ba24110d0a59e72859
Kinesis <lordkinesis@naver.com>
2024-07-24 00:03:49 +0900

    :sparkles: Add new variables in game-manager.h

--- HelloMyPixelBots/game-manager.h
+++ HelloMyPixelBots/game-manager.h
@@ -22,6 +22,10 @@ typedef struct _GameManager {
 	int posFinishX; // Finish Point의 position X값.
 	int posFinishY; // Finish Point의 position Y값.
 	boolean isStageClear;
+	int selectChapterName; // 선택한 Chapter 이름.
+	int selectStageName; // 선택한 Stage 이름.
+	char* condition; // PixelBot의 이동 방향 (Up, Down, Left, Right)
+	boolean isPassAPI; // step의 값이 높아 Dialog가 중복으로 생성 되는 것을 방지하기 위함 (문장이 step만큼 반복되어 출력됨).
 
 }GameManager;
 
