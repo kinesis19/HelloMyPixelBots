221ee454eca65deb5a40524c753f66c3a2a361c8
Kinesis <lordkinesis@naver.com>
2024-07-23 15:05:14 +0900

    :wrench: Modify variable name and Add Typing_API_To_Editor feature in editor-manager.h

--- HelloMyPixelBots/editor-manager.h
+++ HelloMyPixelBots/editor-manager.h
@@ -1,5 +1,7 @@
 #pragma once
 typedef struct {
     char* command; // 입력 받은 API 저장 변수.
-    int number; // 입력 받은 API의 Step 등의 상수 저장 변수.
+    int step; // 입력 받은 API의 Step 등의 상수 저장 변수.
 } Command;
+
+void Typing_API_To_Editor();
