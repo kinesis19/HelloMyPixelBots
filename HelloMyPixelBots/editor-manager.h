3d8a349983bc1813f4ebaa8593bcff5e7608c40e
Kinesis <lordkinesis@naver.com>
2024-07-23 20:45:45 +0900

    :wrench: Modify variable name in editor-manager.h

--- HelloMyPixelBots/editor-manager.h
+++ HelloMyPixelBots/editor-manager.h
@@ -1,6 +1,6 @@
 #pragma once
 typedef struct {
-    char* command[100]; // 입력 받은 API 저장 변수. (명령어 길이 Maximum : 100)
+    char command[100]; // 입력 받은 API 저장 변수. (명령어 길이 Maximum : 100)
     int step; // 입력 받은 API의 Step 등의 상수 저장 변수.
 } CommandList;
 
